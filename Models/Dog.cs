2cb3e7a0314bff9e03dd5957b20f25847db2f894
Andreea Hiriczko <andreeea.hiriczko@gmail.com>
2023-01-07 17:36:49 +0200

    Add project files.

---
+++ Models/Dog.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dog_Grooming_Salon.Models
+{
+    public class Dog
+    {
+        public int ID { get; set; }
+        [Display(Name = "Dog Name")]
+        public string Name { get; set; }
+        public string Age { get; set; }
+
+        public int? OwnerID { get; set; }
+        public Owner? Owner { get; set; }
+    }
+}
